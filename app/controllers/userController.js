d20b1a56e4b8fe3f21e58026481e0c732a924638
bikesh <bikesh@abym.in>
2024-09-23 14:55:59 +0530

    new user api node

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -15,9 +15,10 @@ const twilio = require('twilio');
 *** Twilio credentials
 **
 */
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = new twilio(accountSid, authToken);
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = new twilio(accountSid, authToken);
+const client=''
 
 /*
 **
